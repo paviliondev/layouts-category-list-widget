f8a3291ec6f8c1ceeaab44ecd5fc90d19c1a1a92
Robert <35533304+merefield@users.noreply.github.com>
2021-09-07 17:14:32 +0100

    FEATURE: Two letter Category Text Logos (#6)
    * FEATURE: use two letters instead of one for auto gen category logos
    
    * FIX: function typo
    
    * add debugger statement
    
    * FIX: number of chars
    
    * FIX: capitalise text logos

--- javascripts/discourse/widgets/layouts-category-list.js
+++ javascripts/discourse/widgets/layouts-category-list.js
@@ -403,7 +403,7 @@ createWidget('layouts-category-link', {
         attributes: {
           style: `border: 1px solid #${category.color}` 
         }
-      }, category.name.charAt(0));
+      }, category.name.substring(0,2));
     }
 
     contents.push(
