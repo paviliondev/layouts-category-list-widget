893ac1a0cac8c5deb612447a56a339b1743fad49
Keegan George <kgeorge13@gmail.com>
2021-12-07 12:16:41 -0800

    DEV: Apply Prettier 💄

--- javascripts/discourse/widgets/layouts-category-list.js
+++ javascripts/discourse/widgets/layouts-category-list.js
@@ -1,80 +1,86 @@
-import { createWidget } from 'discourse/widgets/widget';
-import { iconNode } from "discourse-common/lib/icon-library";
-import { h } from 'virtual-dom';
+import { iconNode } from 'discourse-common/lib/icon-library';
 import DiscourseURL from 'discourse/lib/url';
-import Topic from "discourse/models/topic";
-import { set } from "@ember/object";
-import I18n from "I18n";
+import { createWidget } from 'discourse/widgets/widget';
+import I18n from 'I18n';
+import { h } from 'virtual-dom';
 
 let layouts;
 
 try {
-  layouts = requirejs('discourse/plugins/discourse-layouts/discourse/lib/layouts');
-} catch(error) {
+  layouts = requirejs(
+    'discourse/plugins/discourse-layouts/discourse/lib/layouts'
+  );
+} catch (error) {
   layouts = { createLayoutsWidget: createWidget };
   console.warn(error);
 }
 
 const isOlderThanXMonths = (category, count) => {
-  return moment(category.bumped_at).isBefore(moment().subtract(count, 'months'));
+  return moment(category.bumped_at).isBefore(
+    moment().subtract(count, 'months')
+  );
 };
 
 export default layouts.createLayoutsWidget('category-list', {
-  
   defaultState(attrs) {
-    const topicTracking = this.register.lookup("topic-tracking-state:main");
-    
+    const topicTracking = this.register.lookup('topic-tracking-state:main');
+
     return {
       hideExtraChildren: true,
       topicTracking,
-      hideChildren: {}
-    }
+      hideChildren: {},
+    };
   },
-  
+
   didRenderWidget() {
     const self = this;
-    this.state.topicTracking.addObserver('states', function() {
+    this.state.topicTracking.addObserver('states', function () {
       self.scheduleRerender();
     });
   },
-  
+
   isCurrent(refCat) {
     const { category } = this.attrs;
-    return category && (category.id == refCat.id);
+    return category && category.id == refCat.id;
   },
-  
+
   isParentOfCurrent(refCat) {
     const { category } = this.attrs;
-    return category &&
+    return (
+      category &&
       category.parentCategory &&
-      (category.parentCategory.id == refCat.id);
+      category.parentCategory.id == refCat.id
+    );
   },
-  
+
   isGrandparentOfCurrent(refCat) {
     const { category } = this.attrs;
-    return category &&
+    return (
+      category &&
       category.parentCategory &&
       category.parentCategory.parentCategory &&
-      (category.parentCategory.parentCategory.id == refCat.id);
+      category.parentCategory.parentCategory.id == refCat.id
+    );
   },
-  
+
   getChildren(category) {
     const { childCategories } = this.attrs;
-    if (!childCategories || !category || !childCategories[category.slug]) return [];
+    if (!childCategories || !category || !childCategories[category.slug])
+      return [];
     return this.filterExclusions(childCategories[category.slug]);
   },
-  
+
   getParents() {
     const { parentCategories } = this.attrs;
     if (!parentCategories) return [];
     return this.filterExclusions(parentCategories);
   },
-  
+
   filterExclusions(list) {
     const excluded = settings.excluded_categories.split('|');
-    return list.filter(c => excluded.indexOf(c.slug) === -1);
+    return list.filter((c) => excluded.indexOf(c.slug) === -1);
   },
-  
+
   html(attrs, state) {
     const { category, mobileView, tabletView } = attrs;
     const categories = this.getParents();
@@ -88,14 +94,15 @@ export default layouts.createLayoutsWidget('category-list', {
       list.push(this.buildHeader(categoryHeader));
     }
 
-    categories.forEach(category => {
+    categories.forEach((category) => {
       this.addCategory(list, category);
     });
 
     this.addCustomLinks(list);
 
     if (!mobileView && settings.collapsible_sidebar) {
-      let top = tabletView || settings.collapsible_sidebar_desktop_toggle === 'top';
+      let top =
+        tabletView || settings.collapsible_sidebar_desktop_toggle === 'top';
       attrs.position = top ? 'top' : 'bottom';
       let minimizeButton = this.attach('layouts-minimize-categories', attrs);
 
@@ -112,13 +119,13 @@ export default layouts.createLayoutsWidget('category-list', {
   addCustomLinks(list) {
     if (settings.custom_links) {
       const customLinks = [];
-      settings.custom_links.split('|').map(link => {
+      settings.custom_links.split('|').map((link) => {
         let linkItems = link.split(',');
         let linkItem = {
           title: linkItems[0],
           icon: linkItems[1],
-          url: linkItems[2]
-        }
+          url: linkItems[2],
+        };
 
         let options = {};
         if (linkItems[3]) {
@@ -135,34 +142,35 @@ export default layouts.createLayoutsWidget('category-list', {
         return customLinks.push(linkItem);
       });
 
-      const linkHeaderBelow = this.linkHeader("below");
+      const linkHeaderBelow = this.linkHeader('below');
       if (linkHeaderBelow) {
         list.push(this.buildHeader(linkHeaderBelow));
       }
 
-      customLinks.forEach(link => {
+      customLinks.forEach((link) => {
         let linkWidget = this.attach('layouts-custom-link', { link });
 
-        if (link.options.location === "below") {
+        if (link.options.location === 'below') {
           list.push(linkWidget);
         } else {
           list.unshift(linkWidget);
         }
       });
 
-      const linkHeaderAbove = this.linkHeader("above");
+      const linkHeaderAbove = this.linkHeader('above');
       if (linkHeaderAbove) {
         list.unshift(this.buildHeader(linkHeaderAbove));
       }
     }
   },
 
-  addCategory(list, category, child=false) {
+  addCategory(list, category, child = false) {
     const { topicTracking, side, hideChildren } = this.state;
     const { sidebarMinimized } = this.attrs;
     const children = this.getChildren(category);
     const active = this.isCurrent(category);
-    const current = this.isCurrent(category) ||
+    const current =
+      this.isCurrent(category) ||
       this.isParentOfCurrent(category) ||
       this.isGrandparentOfCurrent(category);
     const hasChildren = children.length > 0;
@@ -180,13 +188,14 @@ export default layouts.createLayoutsWidget('category-list', {
         hasChildren,
         showChildren,
         sidebarMinimized,
-        customLogoUrl
+        customLogoUrl,
       })
     );
 
     if (showChildren) {
       list.push(
-        h(`ul.child-categories${!!child ? '.grandchildren' : ''}`,
+        h(
+          `ul.child-categories${!!child ? '.grandchildren' : ''}`,
           this.buildChildList(children, category)
         )
       );
@@ -206,21 +215,31 @@ export default layouts.createLayoutsWidget('category-list', {
 
     list.some((c, index) => {
       if (c.seperator) {
-        if (hideExtraChildren && c.seperator == 1 && index < (list.length - 1)) {
-          result.push(h('li.show-more', this.attach('button', {
-            action: 'showExtraChildren',
-            label: 'more',
-            className: 'btn-small show-extra-children'
-          })));
+        if (hideExtraChildren && c.seperator == 1 && index < list.length - 1) {
+          result.push(
+            h(
+              'li.show-more',
+              this.attach('button', {
+                action: 'showExtraChildren',
+                label: 'more',
+                className: 'btn-small show-extra-children',
+              })
+            )
+          );
           return true;
         } else {
-          result.push(h("li.time-gap", [
-            h('span'),
-            h('label', I18n.t("dates.tiny.x_months", {
-              count: Number(c.seperator)
-            })),
-            h('span')
-          ]));
+          result.push(
+            h('li.time-gap', [
+              h('span'),
+              h(
+                'label',
+                I18n.t('dates.tiny.x_months', {
+                  count: Number(c.seperator),
+                })
+              ),
+              h('span'),
+            ])
+          );
           return false;
         }
       } else {
@@ -254,14 +273,18 @@ export default layouts.createLayoutsWidget('category-list', {
   },
 
   orderByActivity(list) {
-    list = list.filter(c => c.bumped_at)
-      .sort((a,b) => (new Date(b.bumped_at) - new Date(a.bumped_at)))
-  
-    let monthSeperators = {1:false,2:false,4:false,6:false};
+    list = list
+      .filter((c) => c.bumped_at)
+      .sort((a, b) => new Date(b.bumped_at) - new Date(a.bumped_at));
+
+    let monthSeperators = { 1: false, 2: false, 4: false, 6: false };
     list.forEach((category, index) => {
       let addSeperator = null;
       Object.keys(monthSeperators).forEach((seperator) => {
-        if (!monthSeperators[seperator] && isOlderThanXMonths(category, seperator)) {
+        if (
+          !monthSeperators[seperator] &&
+          isOlderThanXMonths(category, seperator)
+        ) {
           monthSeperators[seperator] = true;
           addSeperator = seperator;
         }
@@ -270,7 +293,7 @@ export default layouts.createLayoutsWidget('category-list', {
         list.splice(index, 0, { seperator: addSeperator });
       }
     });
-      
+
     return list;
   },
 
@@ -285,46 +308,52 @@ export default layouts.createLayoutsWidget('category-list', {
   },
 
   customHeaders() {
-    return settings.custom_headers.split('|').map(setting => {
+    return settings.custom_headers.split('|').map((setting) => {
       let parts = setting.split(',');
       let options = parts[1] ? parts[1].split(':') : [];
       return {
         label: parts[0],
         type: options[0],
         position: options[1],
-        link: parts[2]
-      }
+        link: parts[2],
+      };
     });
   },
 
   categoryHeader() {
     const headers = this.customHeaders();
-    const categoryHeaders = headers.filter(h => h.type === "categories");
+    const categoryHeaders = headers.filter((h) => h.type === 'categories');
     return categoryHeaders.length ? categoryHeaders[0] : null;
   },
 
   linkHeader(position) {
     const headers = this.customHeaders();
-    const linkHeaders = headers.filter(h => h.type === "links" && h.position === position);
+    const linkHeaders = headers.filter(
+      (h) => h.type === 'links' && h.position === position
+    );
     return linkHeaders.length ? linkHeaders[0] : null;
   },
 
   buildHeader(header) {
     const { sidebarMinimized } = this.attrs;
     let headerContent = sidebarMinimized ? '' : header.label;
-    let contents = h('h3', { attributes: { title: header.label }}, headerContent);
+    let contents = h(
+      'h3',
+      { attributes: { title: header.label } },
+      headerContent
+    );
 
     if (header.link) {
-      contents = h('a', { attributes: { href: header.link }}, contents);
+      contents = h('a', { attributes: { href: header.link } }, contents);
     }
-  
+
     let extraClasses = '';
     if (!header.link) {
       extraClasses += '.no-link';
     }
-  
+
     return h(`li.layouts-category-link.header${extraClasses}`, contents);
-  }
+  },
 });
 
 createWidget('layouts-minimize-categories', {
@@ -346,10 +375,20 @@ createWidget('layouts-minimize-categories', {
     let result = [];
 
     let iconClasses = sidebarMinimized ? 'minimized' : '';
-    let iconKey = tabletView ? (sidebarMinimized ? 'bars' : 'times') : 'chevron-circle-left';
-    let icon = h(`div.category-logo.minimize-icon.${iconClasses}`, iconNode(iconKey));
-    let textKey = tabletView ? "menu_button_label" : "minimize_button_label";
-    let text = h('div.category-name.minimize-text', I18n.t(themePrefix(textKey)));
+    let iconKey = tabletView
+      ? sidebarMinimized
+        ? 'bars'
+        : 'times'
+      : 'chevron-circle-left';
+    let icon = h(
+      `div.category-logo.minimize-icon.${iconClasses}`,
+      iconNode(iconKey)
+    );
+    let textKey = tabletView ? 'menu_button_label' : 'minimize_button_label';
+    let text = h(
+      'div.category-name.minimize-text',
+      I18n.t(themePrefix(textKey))
+    );
 
     if (tabletView) {
       result.push(text);
@@ -376,10 +415,10 @@ createWidget('layouts-minimize-categories', {
       side: this.attrs.side,
       value: !this.attrs.sidebarMinimized,
       target: 'desktop',
-      type: 'minimize'
+      type: 'minimize',
     });
-  }
-})
+  },
+});
 
 createWidget('layouts-category-link', {
   tagName: 'li',
@@ -396,8 +435,8 @@ createWidget('layouts-category-link', {
 
     return {
       extraClasses: [],
-      showLatest: setCats.filter(c => refCats.indexOf(c) > -1).length
-    }
+      showLatest: setCats.filter((c) => refCats.indexOf(c) > -1).length,
+    };
   },
 
   buildAttributes() {
@@ -406,11 +445,11 @@ createWidget('layouts-category-link', {
     const attributes = {};
 
     if (sidebarMinimized) {
-      attributes["data-tooltip"] = category.name;
+      attributes['data-tooltip'] = category.name;
     }
 
-    attributes["aria-label"] = category.name;
-    attributes["title"] = category.name;
+    attributes['aria-label'] = category.name;
+    attributes['title'] = category.name;
 
     return attributes;
   },
@@ -440,12 +479,12 @@ createWidget('layouts-category-link', {
       showChildren,
       sidebarMinimized,
       current,
-      customLogoUrl
+      customLogoUrl,
     } = attrs;
     let contents = [];
     let logoContents;
     let logoUrl;
-    let lockIcon = settings.category_lock_icon || "lock"
+    let lockIcon = settings.category_lock_icon || 'lock';
 
     if (customLogoUrl) {
       logoUrl = customLogoUrl;
@@ -456,8 +495,8 @@ createWidget('layouts-category-link', {
     if (logoUrl) {
       logoContents = h('img', {
         attributes: {
-          src: logoUrl
-        }
+          src: logoUrl,
+        },
       });
 
       if (state.extraClasses.indexOf('has-logo') === -1) {
@@ -465,42 +504,51 @@ createWidget('layouts-category-link', {
         this.scheduleRerender();
       }
     } else if (settings.auto_generate_category_logos) {
-      logoContents = h('span.category-text-logo', { 
-        attributes: {
-          style: `border: 1px solid #${category.color}` 
-        }
-      }, category.name.substring(0,2));
+      logoContents = h(
+        'span.category-text-logo',
+        {
+          attributes: {
+            style: `border: 1px solid #${category.color}`,
+          },
+        },
+        category.name.substring(0, 2)
+      );
     } else if (settings.category_badges) {
-      logoContents = h(`span.badge-wrapper.${category.siteSettings.category_style}`, h('span.badge-category-bg', {
-        attributes: {
-          style: `background-color: #${category.color}`
-        }
-      }));
+      logoContents = h(
+        `span.badge-wrapper.${category.siteSettings.category_style}`,
+        h('span.badge-category-bg', {
+          attributes: {
+            style: `background-color: #${category.color}`,
+          },
+        })
+      );
     }
 
     contents.push(
-      h(`div.category-logo.${category.slug}`, {
-        attributes: {
-          "data-category-id": category.id
-        }
-      }, logoContents)
-    )  
-    
-    if (category.read_restricted) {
-      contents.push(iconNode(lockIcon, { class: "category-lock-icon" }));
-    }
-    
-    contents.push(
-      h('div.category-name', category.name)
+      h(
+        `div.category-logo.${category.slug}`,
+        {
+          attributes: {
+            'data-category-id': category.id,
+          },
+        },
+        logoContents
+      )
     );
-    
+
+    if (category.read_restricted) {
+      contents.push(iconNode(lockIcon, { class: 'category-lock-icon' }));
+    }
+
+    contents.push(h('div.category-name', category.name));
+
     const latestTopicCount = topicTracking.lookupCount('latest', category);
-    if (state.showLatest && latestTopicCount > 0) {      
+    if (state.showLatest && latestTopicCount > 0) {
       contents.push(
         h('div.badge-notification.new-posts', `${latestTopicCount}`)
       );
     }
-    
+
     if (hasChildren && !sidebarMinimized) {
       let actionParam = { category };
 
@@ -513,35 +561,37 @@ createWidget('layouts-category-link', {
           icon: showChildren ? 'chevron-up' : 'chevron-down',
           action: 'toggleChildren',
           actionParam,
-          className: 'toggle-children'
+          className: 'toggle-children',
         })
       );
     }
-    
+
     if (attrs.active) {
-      contents.push(h('span.active-marker', { 
-        attributes: { style: `background-color: #${category.color}` }
-      }))
+      contents.push(
+        h('span.active-marker', {
+          attributes: { style: `background-color: #${category.color}` },
+        })
+      );
     }
-    
+
     return contents;
   },
-  
+
   click() {
     if (!this.attrs.hasChildren) {
       this.appEvents.trigger('sidebar:toggle', {
         side: this.attrs.side,
         value: false,
-        target: 'mobile'
+        target: 'mobile',
       });
     }
     this.sendWidgetAction('toggleChildren', {
       category: this.attrs.category,
-      hideChildren: false
+      hideChildren: false,
     });
     return true;
-  }
-})
+  },
+});
 
 createWidget('layouts-custom-link', {
   tagName: 'li',
@@ -550,7 +600,10 @@ createWidget('layouts-custom-link', {
     const { link } = attrs;
     let classes = 'layouts-custom-link layouts-category-link';
 
-    let data = Object.assign({}, document.getElementById("data-discourse-setup").dataset);
+    let data = Object.assign(
+      {},
+      document.getElementById('data-discourse-setup').dataset
+    );
     let baseUrl = data && data.baseUrl ? data.baseUrl : '';
     let url = baseUrl + link.url;
     let currentUrl = window.location.href;
@@ -569,18 +622,21 @@ createWidget('layouts-custom-link', {
       'div.category-name',
       {
         attributes: {
-          title: link.title
-        }
+          title: link.title,
+        },
       },
       link.title
     );
 
-    let icon = h('div.category-logo', h('img', {
-      attributes: {
-        src: link.icon,
-        alt: link.title
-      }
-    }));
+    let icon = h(
+      'div.category-logo',
+      h('img', {
+        attributes: {
+          src: link.icon,
+          alt: link.title,
+        },
+      })
+    );
 
     result.push(icon, title);
     return result;
@@ -595,5 +651,5 @@ createWidget('layouts-custom-link', {
     } else {
       DiscourseURL.routeTo(url);
     }
-  }
-})
+  },
+});
